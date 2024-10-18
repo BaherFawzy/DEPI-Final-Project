1aa35a554e9342ed2a9f81b73fa670485e41fb5b
Mahmoud Sharara <mahmoodshrara@gmail.com>
2024-10-18 11:50:11 +0300

    edit jenkinsfile

--- k8s/helm/ArgoCD/deploy-argocd-minikube.sh
+++ k8s/helm/ArgoCD/deploy-argocd-minikube.sh
@@ -1,34 +1,26 @@
-#!/bin/bash
+#/bin/bash
 
-# Create Namespace
+#Create Namespace
 echo "--------------------Create Argocd Namespace--------------------"
 kubectl create ns argocd || true
 
-# Deploy ArgoCD
+#Deploy Argocd
 echo "--------------------Deploy Argocd--------------------"
-# Check if ArgoCD components are already up-to-date or need to be applied
-if kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml | grep -q 'unchanged'; then
-    echo "No changes detected in ArgoCD deployment. Skipping reapply."
-else
-    echo "Changes detected in ArgoCD deployment. Reapplying..."
-    # Force reapply of ArgoCD components
-    kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml --force
-fi
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
-# Wait 1 minute for the pods to start
+#Sleep 1 miniute
 echo "--------------------Waiting 1m for the pods to start--------------------"
 sleep 1m
 
-# Change ArgoCD Service to NodePort
+#Change to Nodeport
 echo "--------------------Change Argocd Service to NodePort--------------------"
 kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "NodePort"}}'
 
-# ArgoCD URL
+#ArgoCD URL
 echo "--------------------ArgoCD URL--------------------"
 minikube service -n argocd argocd-server --url
 
-# ArgoCD UI Password
+#ArgoCD Pass
 echo "--------------------ArgoCD UI Password--------------------"
 echo "Username: admin"
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d > argo-pass.txt
-cat argo-pass.txt
