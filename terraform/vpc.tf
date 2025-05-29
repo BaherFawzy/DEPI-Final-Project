cec7deb770793ca766f1a8e4e9c7e5aacc64e166
Mahmoud Sharara <61325916+sharara99@users.noreply.github.com>
2025-05-29 16:31:40 +0300

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -25,8 +25,8 @@ resource "aws_subnet" "subnet_public_1" {
 
 resource "aws_subnet" "subnet_public_2" {
   vpc_id                  = aws_vpc.main_vpc.id
-  cidr_block              = cidrsubnet(var.vpc_cidr, 32, 2)
-  availability_zone       = "us-east-1a"
+  cidr_block              = cidrsubnet(var.vpc_cidr, 8, 1)
+  availability_zone       = "us-east-1b" # different AZ for HA
   map_public_ip_on_launch = true
 
   tags = {
